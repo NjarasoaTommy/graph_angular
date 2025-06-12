3d197bb1e5852106b30b59ff95693144bbdd119d
Njarasoa Tommy <njarasoatommy@gmail.com>
2025-06-12 18:21:40 +0300

    Create my first graphe using ngx-graph

---
+++ src/app/first-graph/first-graph.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { NgxGraphModule } from '@swimlane/ngx-graph';
+
+@Component({
+  selector: 'app-first-graph',
+  imports: [NgxGraphModule],
+  templateUrl: './first-graph.component.html',
+  styleUrl: './first-graph.component.css',
+})
+export class FirstGraphComponent {}
