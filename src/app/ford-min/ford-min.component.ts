00932ba6b5aeb34cf9f13a42aa45322c796a5ab5
Njarasoa Tommy <njarasoatommy@gmail.com>
2025-06-13 19:46:05 +0300

    Use ngx-graph to generate node and arc(link). Also use subject to detect change automatically

--- src/app/ford-min/ford-min.component.ts
+++ src/app/ford-min/ford-min.component.ts
@@ -1,11 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { NgxGraphModule, Node, Edge } from '@swimlane/ngx-graph';
+import { Subject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-ford-min',
-  imports: [],
+  imports: [NgxGraphModule],
   templateUrl: './ford-min.component.html',
-  styleUrl: './ford-min.component.css'
+  styleUrl: './ford-min.component.css',
 })
-export class FordMinComponent {
+export class FordMinComponent implements OnInit {
+  my_links: Edge[] = [];
+  my_nodes: Node[] = [];
+  my_links_subject = new Subject<Edge[]>();
+  my_nodes_subject = new Subject<Node[]>();
 
+  my_nodes_subscription: Subscription | null = null;
+  my_links_subscription: Subscription | null = null;
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  ngOnInit() {
+    this.my_links_subscription = this.my_links_subject.subscribe(
+      (links: Edge[]) => {
+        this.my_links = links;
+      }
+    );
+    this.my_nodes_subscription = this.my_nodes_subject.subscribe(
+      (nodes: Node[]) => {
+        this.my_nodes = nodes;
+      }
+    );
+
+    this.addNode('Node 1');
+    this.addNode('Node 2');
+    this.addLink('1', '2');
+  }
+
+  emitNodeSubject() {
+    // this.my_nodes = [...this.my_nodes];
+    // this.my_nodes_subject.next(this.my_nodes);
+    this.my_nodes_subject.next(this.my_nodes.slice()); //copy of nodes list
+  }
+  emitLinkSubject() {
+    this.my_links = [...this.my_links];
+    this.my_links_subject.next(this.my_links); //copy of links list
+    // this.my_links_subject.next(this.my_links.slice()); //copy of links list
+  }
+
+  addNode(label: string = '') {
+    this.my_nodes.push({
+      id: (this.my_nodes.length + 1).toString(),
+      label: label,
+    });
+    this.cdr.detectChanges();
+    console.log(this.my_nodes);
+    this.emitNodeSubject();
+  }
+
+  addLink(source: string, target: string) {
+    this.my_links.push({
+      id: 'link' + (this.my_links.length + 1).toString(),
+      source: source,
+      target: target,
+    });
+    console.log(this.my_links);
+    this.emitLinkSubject();
+  }
 }
